1305853ef8bc1802e088a14d91e6d2177e67c998
OlafOnFire <96726371+OlafOnFire@users.noreply.github.com>
2022-09-07 08:51:04 +0200

    Added Networking for all deployments

--- linux-prd/skripte/LINUX-HH-DC-PRD/setup.sh
+++ linux-prd/skripte/LINUX-HH-DC-PRD/setup.sh
@@ -5,5 +5,5 @@ sudo apt update && apt upgrade -y && apt dist-upgrade -y
 sudo loadkeys de
 
 #Install dependencies for AD / DC
-sudo apt -y install samba krb5-config winbind smbclient
+# sudo apt -y install samba krb5-config winbind smbclient
 echo Test
